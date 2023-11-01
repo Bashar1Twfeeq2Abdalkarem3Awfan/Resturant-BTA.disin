ec11482f9baaa469b0760797bd8def5fba12bd5f
Bashar Twfeeq AbdAl-karem Awfan <149325007+Bashar1Twfeeq2Abdalkarem3Awfan@users.noreply.github.com>
2023-11-01 07:23:08 +0300

    Add files via upload

---
+++ js/boton_maseg.js
@@ -0,0 +1,15 @@
+let butop=document.getElementById("loog");
+butop.innerText="Click here";
+let box=document.getElementById("ho");
+box.style.display="none";
+/*butop.addEventListener("click",()=>{
+    home.classList. add("show")
+})
+butop.addEventListener("click",()=>{
+    home.classList. remove("show")
+})*/
+function tool(){
+    box.style.display=box.style.display==="none" ? "block":"none";
+
+}
+butop.addEventListener("click",tool);
